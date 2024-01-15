d77391a5d31a6f466f0b74bf32aaacc698933743
Rybim <118078619+Agent-Lemm1ngs@users.noreply.github.com>
2024-01-15 20:00:34 +0000

    fix: Fix an issue with the PrismaAdapter and remove unused imports and whitespace

--- quizal/app/api/auth/[...nextauth]/route.ts
+++ quizal/app/api/auth/[...nextauth]/route.ts
@@ -4,10 +4,9 @@ import bcrypt from "bcrypt";
 import { PrismaAdapter } from "@auth/prisma-adapter";
 import { PrismaClient } from "@prisma/client";
 const prisma = new PrismaClient();
-import type { Adapter } from "next-auth/adapters";
 
 const authOptions = {
-  adapter: PrismaAdapter(prisma) as Adapter,
+  adapter: PrismaAdapter(prisma),
   providers: [
     CredentialsProvider({
       name: "Credentials",
