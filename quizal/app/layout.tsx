73118657098656b89371df2c3e60b9b14ab680d6
Rybim <118078619+Agent-Lemm1ngs@users.noreply.github.com>
2024-01-15 22:15:56 +0000

    Update layout.tsx

--- quizal/app/layout.tsx
+++ quizal/app/layout.tsx
@@ -23,7 +23,7 @@ export default async function RootLayout({
 }: {
   children: React.ReactNode;
 }) {
-  const session = await getServerSession();
+  const session = (await getServerSession()) || null;
   return (
     <html lang="en">
       <Provider session={session}>
