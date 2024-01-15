9c97586c8101470d1b7126c28c862f98ac66df8a
Rybim <118078619+Agent-Lemm1ngs@users.noreply.github.com>
2024-01-15 20:40:40 +0000

    fix: Fix import statement for next-auth session

--- quizal/app/context/provider.tsx
+++ quizal/app/context/provider.tsx
@@ -1,6 +1,6 @@
 "use client";
 import { SessionProvider } from "next-auth/react";
-import type { Session } from "next-auth";
-export default function Provider({ children: ReactNode, session: Session }) {
+import type { Session } from "next-auth/";
+export default function Provider({ children, session: Session }) {
   return <SessionProvider session={session}>{children}</SessionProvider>;
 }
