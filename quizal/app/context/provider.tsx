72d4e376c528b4e143e74a5e4d3c4d51c51b3f68
Rybim <118078619+Agent-Lemm1ngs@users.noreply.github.com>
2024-01-15 21:31:55 +0000

    feat: Format code, import correct type for Session and add interface for ProviderProps

--- quizal/app/context/provider.tsx
+++ quizal/app/context/provider.tsx
@@ -1,6 +1,13 @@
 "use client";
 import { SessionProvider } from "next-auth/react";
-import type { Session } from "next-auth/";
-export default function Provider({ children, session }, props) {
+import type { ReactNode } from "react";
+import type { Session } from "next-auth/client";
+
+interface ProviderProps {
+  children: ReactNode;
+  session: Session;
+}
+
+export default function Provider({ children, session }: ProviderProps) {
   return <SessionProvider session={session}>{children}</SessionProvider>;
 }
