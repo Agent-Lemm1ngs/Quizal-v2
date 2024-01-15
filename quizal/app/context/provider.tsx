794105cce8139f4ed9d529383f30019b49159cca
Rybim <118078619+Agent-Lemm1ngs@users.noreply.github.com>
2024-01-15 20:38:18 +0000

    feat: Add type annotations for Session in Provider and format code in Provider

--- quizal/app/context/provider.tsx
+++ quizal/app/context/provider.tsx
@@ -1,5 +1,6 @@
 "use client";
 import { SessionProvider } from "next-auth/react";
-export default function Provider({ children: children, session: session }) {
+import type { Session } from "next-auth";
+export default function Provider({ children: ReactNode, session: Session }) {
   return <SessionProvider session={session}>{children}</SessionProvider>;
 }
