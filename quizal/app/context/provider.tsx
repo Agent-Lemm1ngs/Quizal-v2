1d0a7477a060a797feefce9b3c5c6a3502c3e4de
Rybim <118078619+Agent-Lemm1ngs@users.noreply.github.com>
2024-01-15 20:43:56 +0000

    Update provider.tsx

--- quizal/app/context/provider.tsx
+++ quizal/app/context/provider.tsx
@@ -1,6 +1,6 @@
 "use client";
 import { SessionProvider } from "next-auth/react";
 import type { Session } from "next-auth/";
-export default function Provider({ children, session: Session }) {
+export default function Provider({ children, session }, props) {
   return <SessionProvider session={session}>{children}</SessionProvider>;
 }
