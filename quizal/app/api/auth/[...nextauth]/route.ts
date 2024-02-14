58b9d7251783f605a681d6f628c2df09e8d14a37
Rybim <118078619+Agent-Lemm1ngs@users.noreply.github.com>
2024-02-14 19:17:53 +0000

    fix: Fix export statement in authOptions

--- quizal/app/api/auth/[...nextauth]/route.ts
+++ quizal/app/api/auth/[...nextauth]/route.ts
@@ -3,7 +3,7 @@ import GoogleProvider from "next-auth/providers/google";
 import { connectDB } from "@/utils/database";
 import User from "@/models/user";
 
-export const authOptions = {
+const authOptions = {
   providers: [
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID,
