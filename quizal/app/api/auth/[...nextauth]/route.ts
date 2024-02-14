6878131033760c68391fd25a270191ac91acc69a
Rybim <118078619+Agent-Lemm1ngs@users.noreply.github.com>
2024-02-14 20:09:15 +0000

    fix: Refactor code style in auth route and fix type casting in auth route

--- quizal/app/api/auth/[...nextauth]/route.ts
+++ quizal/app/api/auth/[...nextauth]/route.ts
@@ -6,8 +6,8 @@ import User from "@/models/user";
 const authOptions = {
   providers: [
     GoogleProvider({
-      clientId: process.env.GOOGLE_CLIENT_ID,
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+      clientId: process.env.GOOGLE_CLIENT_ID as string,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
     }),
   ],
   callbacks: {
